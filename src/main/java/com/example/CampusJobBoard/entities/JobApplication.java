727f82c7704bf6f6377a3ed8da463fbccefbc6a6
Jaren-Wheeler <git@github.com>
2025-11-19 13:35:50 -0700

    Created  job and job application entities. Formed relationships between all entities

--- src/main/java/com/example/CampusJobBoard/entities/JobApplication.java
+++ src/main/java/com/example/CampusJobBoard/entities/JobApplication.java
@@ -1,4 +1,56 @@
 package com.example.CampusJobBoard.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="JobApplication")
 public class JobApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long jobApplicationId;
+
+    public enum status {
+        SUBMITTED, ACCEPTED, REJECTED
+    }
+
+    // connect to user table
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="UserId")
+    private User user;
+
+    // connect to job table
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="JobId")
+    private Job job;
+
+
+
+    // GETTERS AND SETTERS //
+
+    public Long getJobApplicationId() {
+        return jobApplicationId;
+    }
+
+    public void setJobApplicationId(Long jobApplicationId) {
+        this.jobApplicationId = jobApplicationId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+
 }
