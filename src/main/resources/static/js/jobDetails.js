f1e4208f12a72fb6a6d326d4d8aeca83173301fd
Jaren-Wheeler <git@github.com>
2025-11-18 21:06:16 -0700

    Created application modal and buttons to submit, go back to details, and close window

--- src/main/resources/static/js/jobDetails.js
+++ src/main/resources/static/js/jobDetails.js
@@ -1,10 +1,31 @@
 
+// opens the details modal for the specific job clicked
 function openModal(id) {
     let modal = document.getElementById("jobModal");
     modal.classList.remove("hidden");
 }
 
+//closes the details modal
 function closeModal() {
     let modal = document.getElementById("jobModal");
     modal.classList.add("hidden");
+}
+
+// opens the modal to create a job application
+function openApplyModal() {
+    let applyModal = document.getElementById("applyModal");
+    applyModal.classList.remove("hidden");
+}
+
+// closes both modals at one (create application and details)
+function closeApplyModal() {
+    let applyModal = document.getElementById("applyModal");
+    applyModal.classList.add("hidden");
+    closeModal();
+}
+
+// go back to details window from apply window
+function returnToDetails() {
+    let applyModal = document.getElementById("applyModal");
+    applyModal.classList.add("hidden");
 }
